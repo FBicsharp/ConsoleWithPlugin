5516b7e14abeae746fb80b7f94f56517cf972dc4
fbicciato <f.bicciato.lavoro@gmail.com>
2022-03-23 16:56:24 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleWithPlugin/PluginLoader.cs
@@ -0,0 +1,47 @@
+﻿using PluginBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWithPlugin
+{
+    public class PluginLoader
+    {
+        public static List<ICommand> Plugins { get; set; }
+
+        public void LoadPlugins(string folder)
+        {
+            Plugins = new List<ICommand>();
+
+            //Load the DLLs from the Plugins directory
+            if (Directory.Exists(folder))
+            {
+                string[] files = Directory.GetFiles(folder);
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".dll"))
+                    {
+                        Assembly.LoadFile(Path.GetFullPath(file));
+                    }
+                }
+            }
+
+            Type interfaceType = typeof(ICommand);
+            //Fetch all types that implement the interface ICommand and are a class
+            Type[] types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(p => interfaceType.IsAssignableFrom(p) && p.IsClass)
+                .ToArray();
+            foreach (Type type in types)
+            {
+                //Create a new instance of all found types
+                Plugins.Add((ICommand)Activator.CreateInstance(type));
+            }
+        }
+    }
+}
---
+++ ConsoleWithPlugin/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using PluginBase;
+
+namespace ConsoleWithPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Started plugin app..");
+                try
+                {
+                    PluginLoader loader = new PluginLoader();
+                    loader.LoadPlugins("C:\\Plugin");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(string.Format("Plugins couldn't be loaded: {0}", e.Message));
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+
+                while (true)
+                {
+                    try
+                    {                        
+                        Console.Write("> ");
+                        string line = Console.ReadLine();
+                        if (line == "exit")
+                        {
+                            Environment.Exit(0);
+                        }
+
+                    List<Task<int>> tasklist = new List<Task<int>>() ;
+                    PluginLoader.Plugins.ForEach((p) =>
+                        tasklist.Add(
+                            Task.Factory.StartNew(() => p.Execute())
+                        )
+                    );
+                    Task.WaitAll(tasklist.ToArray());
+
+                       
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(string.Format("Caught exception: {0}", e.Message));
+                    }
+                }
+            
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
